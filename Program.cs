aac7142330a6a381f17efa2e54759f5b49d48058
nick <teamsupport1@thelab.site>
2020-02-17 18:05:51 +0100

    Setup practice test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoogleHashCode2020_PracticeTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
